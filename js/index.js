48329e5d592a7eeb902475da3e36c3da4e2fbfe7
Souvik Maity <souvikmaity599@gmail.com>
2023-03-15 13:41:40 +0530

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,152 @@
+
+// ---Problem-Section---
+function myF1() {
+    document.getElementById("i-1").style.display = "none";
+    document.getElementById("i-2").style.display = "block";
+}
+function myF2() {
+    document.getElementById("i-2").style.display = "none";
+    document.getElementById("i-3").style.display = "block";
+}
+function myF3() {
+    document.getElementById("i-3").style.display = "none";
+    document.getElementById("i-4").style.display = "block";
+}
+function myF4() {
+    document.getElementById("i-4").style.display = "none";
+    document.getElementById("i-5").style.display = "block";
+}
+function myF5() {
+    document.getElementById("i-5").style.display = "none";
+    document.getElementById("i-1").style.display = "block";
+}
+
+
+// ---More-Services--- 
+  
+function myFunction1() {
+    document.getElementById("box-1").style.display = "block";
+    document.getElementById("box-2").style.display = "none";
+    document.getElementById("box-3").style.display = "none";
+    document.getElementById("box-4").style.display = "none";
+    document.getElementById("box-5").style.display = "none";
+    document.getElementById("box-6").style.display = "none";
+
+    document.getElementById("ser-1b").style.display = "flex";
+    document.getElementById("ser-1a").style.display = "none";
+    document.getElementById("ser-2b").style.display = "none";
+    document.getElementById("ser-3b").style.display = "none";
+    document.getElementById("ser-4b").style.display = "none";
+    document.getElementById("ser-5b").style.display = "none";
+    document.getElementById("ser-6b").style.display = "none";
+    document.getElementById("ser-2a").style.display = "flex";
+    document.getElementById("ser-3a").style.display = "flex";
+    document.getElementById("ser-4a").style.display = "flex";
+    document.getElementById("ser-5a").style.display = "flex";
+    document.getElementById("ser-6a").style.display = "flex";    
+}
+function myFunction2() {
+    document.getElementById("box-1").style.display = "none";
+    document.getElementById("box-2").style.display = "block";
+    document.getElementById("box-3").style.display = "none";
+    document.getElementById("box-4").style.display = "none";
+    document.getElementById("box-5").style.display = "none";
+    document.getElementById("box-6").style.display = "none";
+    
+    document.getElementById("ser-2b").style.display = "flex";
+    document.getElementById("ser-2a").style.display = "none";
+    document.getElementById("ser-1b").style.display = "none";
+    document.getElementById("ser-3b").style.display = "none";
+    document.getElementById("ser-4b").style.display = "none";
+    document.getElementById("ser-5b").style.display = "none";
+    document.getElementById("ser-6b").style.display = "none";
+    document.getElementById("ser-1a").style.display = "flex";
+    document.getElementById("ser-3a").style.display = "flex";
+    document.getElementById("ser-4a").style.display = "flex";
+    document.getElementById("ser-5a").style.display = "flex";
+    document.getElementById("ser-6a").style.display = "flex";
+}
+function myFunction3() {
+    document.getElementById("box-1").style.display = "none";
+    document.getElementById("box-2").style.display = "none";
+    document.getElementById("box-3").style.display = "block";
+    document.getElementById("box-4").style.display = "none";
+    document.getElementById("box-5").style.display = "none";
+    document.getElementById("box-6").style.display = "none";
+    
+    document.getElementById("ser-3b").style.display = "flex";
+    document.getElementById("ser-3a").style.display = "none";
+    document.getElementById("ser-1b").style.display = "none";
+    document.getElementById("ser-2b").style.display = "none";
+    document.getElementById("ser-4b").style.display = "none";
+    document.getElementById("ser-5b").style.display = "none";
+    document.getElementById("ser-6b").style.display = "none";
+    document.getElementById("ser-1a").style.display = "flex";
+    document.getElementById("ser-2a").style.display = "flex";
+    document.getElementById("ser-4a").style.display = "flex";
+    document.getElementById("ser-5a").style.display = "flex";
+    document.getElementById("ser-6a").style.display = "flex";
+}
+function myFunction4() {
+    document.getElementById("box-1").style.display = "none";
+    document.getElementById("box-2").style.display = "none";
+    document.getElementById("box-3").style.display = "none";
+    document.getElementById("box-4").style.display = "block";
+    document.getElementById("box-5").style.display = "none";
+    document.getElementById("box-6").style.display = "none";
+    
+    document.getElementById("ser-4b").style.display = "flex";
+    document.getElementById("ser-4a").style.display = "none";
+    document.getElementById("ser-1b").style.display = "none";
+    document.getElementById("ser-3b").style.display = "none";
+    document.getElementById("ser-2b").style.display = "none";
+    document.getElementById("ser-5b").style.display = "none";
+    document.getElementById("ser-6b").style.display = "none";
+    document.getElementById("ser-1a").style.display = "flex";
+    document.getElementById("ser-3a").style.display = "flex";
+    document.getElementById("ser-2a").style.display = "flex";
+    document.getElementById("ser-5a").style.display = "flex";
+    document.getElementById("ser-6a").style.display = "flex";
+}
+function myFunction5() {
+    document.getElementById("box-1").style.display = "none";
+    document.getElementById("box-2").style.display = "none";
+    document.getElementById("box-3").style.display = "none";
+    document.getElementById("box-4").style.display = "none";
+    document.getElementById("box-5").style.display = "block";
+    document.getElementById("box-6").style.display = "none";
+    
+    document.getElementById("ser-5b").style.display = "flex";
+    document.getElementById("ser-5a").style.display = "none";
+    document.getElementById("ser-1b").style.display = "none";
+    document.getElementById("ser-3b").style.display = "none";
+    document.getElementById("ser-4b").style.display = "none";
+    document.getElementById("ser-2b").style.display = "none";
+    document.getElementById("ser-6b").style.display = "none";
+    document.getElementById("ser-1a").style.display = "flex";
+    document.getElementById("ser-3a").style.display = "flex";
+    document.getElementById("ser-4a").style.display = "flex";
+    document.getElementById("ser-2a").style.display = "flex";
+    document.getElementById("ser-6a").style.display = "flex";
+}
+function myFunction6() {
+    document.getElementById("box-1").style.display = "none";
+    document.getElementById("box-2").style.display = "none";
+    document.getElementById("box-3").style.display = "none";
+    document.getElementById("box-4").style.display = "none";
+    document.getElementById("box-5").style.display = "none";
+    document.getElementById("box-6").style.display = "block";
+    
+    document.getElementById("ser-6b").style.display = "flex";
+    document.getElementById("ser-6a").style.display = "none";
+    document.getElementById("ser-1b").style.display = "none";
+    document.getElementById("ser-3b").style.display = "none";
+    document.getElementById("ser-4b").style.display = "none";
+    document.getElementById("ser-5b").style.display = "none";
+    document.getElementById("ser-2b").style.display = "none";
+    document.getElementById("ser-1a").style.display = "flex";
+    document.getElementById("ser-3a").style.display = "flex";
+    document.getElementById("ser-4a").style.display = "flex";
+    document.getElementById("ser-5a").style.display = "flex";
+    document.getElementById("ser-2a").style.display = "flex";
+}
